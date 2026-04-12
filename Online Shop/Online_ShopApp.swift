3bceb2669fcbb320625b9e7d7cb81e8bffecc1b9
TrainLeft <dimalistenin070906@gmail.com>
2026-04-12 15:54:20 +0200

    Initial Commit

---
+++ Online Shop/Online_ShopApp.swift
@@ -0,0 +1,17 @@
+//
+//  Online_ShopApp.swift
+//  Online Shop
+//
+//  Created by Dmytro Listenin on 12.04.2026.
+//
+
+import SwiftUI
+
+@main
+struct Online_ShopApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
